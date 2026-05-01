b582291ae2640caab508040bac1ec624f2d617fc
ridwannazim <muhamadridwanwan05@gmail.com>
2026-05-01 16:51:21 +0700

    menambahkan function dan perbaikan struktur

--- projek/kalkulator_sederhana.py
+++ projek/kalkulator_sederhana.py
@@ -1,50 +1,55 @@
-'''hari 12'''
-# kalkulator sederhana
+# kalkulator
 
+def kalkulator(a,b,op):
+    tambah = a+b
+    kurang = a-b
+    kali = a*b
+    bagi = a/b
+    pangkat = a**b
+    floor = a//b
+    modulus = a%b
+    return tambah,kurang,kali,bagi,pangkat,floor,modulus
+def keluar():
+    keluar = input("lanjut ? y/n = ")
+    return keluar == "n"
 while True:
-    print('='*10)
-    angka1 = (input("masukan angka pertama = "))
-    if not angka1.isnumeric():
-        print("salah!! ulangi input")
-        continue
-    operasi = input("masukan operasi = ")
-    if operasi not in ["+","-","*","//","/","**","%"]:
-        print("salah!! ulangi input")
-        continue
-    angka2 = (input("masukan angka kedua = "))
-    if not angka2.isnumeric():
-        print("salah!! ulangi input")
-        continue
+    print("-"*11)
+    print(f"{'kakulator':11}")
+    print("-"*11)
 
-    angka1_1 = float(angka1)
-    angka2_2 = float(angka2)
-    if operasi== "+" :
-        print(f"{angka1_1} + {angka2_2} = {angka1_1 + angka2_2}")
-    elif operasi== "-" :
-        print(f"{angka1_1} - {angka2_2} = {angka1_1 - angka2_2}")
-    elif operasi== "*" :
-        print(f"{angka1_1} * {angka2_2} = {angka1_1 * angka2_2}")
-    elif operasi== "/" :
-        if angka2_2 ==0 : # dalam matematika tidak boleh dibagi dengan angka nol
-            print("gabisa dibagi nol nih boss")
-        else:
-            print(f"{angka1_1} / {angka2_2} = {angka1_1 / angka2_2}")
-    elif operasi== "**" :
-        print(f"{angka1_1} ** {angka2_2} = {angka1_1 ** angka2_2}")
-    elif operasi== "//" :
-        print(f"{angka1_1} // {angka2_2} = {angka1_1 // angka2_2}")
-    elif operasi== "%" :
-        print(f"{angka1_1} % {angka2_2} = {angka1_1 % angka2_2}")
-    else:
-        print("error boosss!!!")
-    
-    while True :
-        pilihan = input("mau lanjut y/n = ").strip().lower()
-        if pilihan == "y":
-            print("="*10)
-            break # untuk menghentikan while ke dua
-        elif pilihan == "n":
-            print("terimakasih")
-            exit()
-        else :
-            print("kesalahan input")
+    angka1 = input(f"{'masukan angka pertama':23}=")
+    if not angka1.isnumeric():
+        print("input salah!!")
+        continue
+    operasi = input(f"{'masukan operasi':23}=")
+    if operasi not in ['+','-','*','/','//','**','%']:
+        print("input salah!!")
+        continue
+    angka2 = input(f"{'masukan angka kedua':23}=")
+    if not angka2.isnumeric():
+        print("input salah!!")
+        continue
+    elif operasi == "/" and angka2 == "0":
+        print("ada kesalahan!! tidak bisa dibagi 0")
+        continue
+    angka1 = float(angka1)
+    angka2 = float(angka2)
+
+    tambah,kurang,kali,bagi,pangkat,floor,modulus = kalkulator(angka1,angka2)
+
+    if operasi == "+":
+        print(f"{angka1} + {angka2} = {tambah}")
+    elif operasi == "-":
+        print(f"{angka1} - {angka2} = {kurang}")
+    elif operasi == "*":
+        print(f"{angka1} * {angka2} = {kali}")
+    elif operasi == "/":
+        print(f"{angka1} / {angka2} = {bagi}")
+    elif operasi == "**":
+        print(f"{angka1} ** {angka2} = {pangkat}")
+    elif operasi == "//":
+        print(f"{angka1} // {angka2} = {floor}")
+    elif operasi == "%":
+        print(f"{angka1} % {angka2} = {modulus}")
+    if keluar():
+        break
