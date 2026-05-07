e8c24cac8b7df7be08521d92f2c0b0a4f868da7b
ridwannazim <muhamadridwanwan05@gmail.com>
2026-05-07 15:33:03 +0700

    refactor menggunakan fungsi

--- projek/kalkulator_BMI.py
+++ projek/kalkulator_BMI.py
@@ -1,56 +1,79 @@
+'''HARI 34'''
+
+# latihan fungsi
+
+import os
 def keluar():
     while True:
-        pilihan = input("mau lanjut (y/n) =").strip().lower()
-        if pilihan == "n" :
+        pilihan = input("mau lanjut (y/n) = ").strip().lower()
+        if pilihan in ["y","n"]  :
             print("Terima kasih sudah menggunakan aplikasi ini!")
-            return True
-        elif not pilihan in ["y","n"] :
-            print("mohon masukan sesaui perintah")
-            return False
+            return pilihan == "n"
+        else:
+            print("input salah!!")        
 
-while True: # Program berjalan terus sampai disuruh berhenti
-    print("\n" + "="*30)
-    nama = input("Nama: ").strip() # Nama otomatis rapi (Andi Ganteng)
-    if nama.lower() == "selesai" :
-        print("Terima kasih sudah menggunakan aplikasi ini!")
-        break
-    nama_input = nama.title()
-    # Input dibersihkan dulu (lowercase dan hapus spasi)
-    gender = input("Gender (Pria/Wanita): ").lower().strip()
-    if not gender in ["pria","cowok","laki laki","cewek","wanita","perempuan"] :
-        print("mohon ulangi")
-        continue
-    tinggi = (input("Tinggi (cm): "))
-    if not tinggi.isnumeric() :
-        print("mohon ulangi")
-        continue
-    berat = (input("Berat (kg): "))
-    if not berat.isnumeric() :
-        print("mohon ulangi")
-        continue
+def header():
+    '''header'''
+    os.system("cls")
+    print(f"{'kalkulator BMI':^30}")
+    print("="*30)
 
+def  input_user():
+    '''input user'''
+    while True:
+        nama = input(F"| {'NAMA':<13}: ").title()
+        gender = input(f"| {'GENDER':<13}: ").lower().replace(" ","")
+        if not gender in ["pria","cowok","lakilaki","cewek","wanita","perempuan"] :
+            print("mohon ulangi")
+            continue
+        tinggi = (input(f"| {'TINGGI (cm)':<13}: "))
+        if not tinggi.isnumeric() :
+            print("mohon ulangi")
+            continue
+        berat = (input(f"| {'BERAT (kg)':<13}: "))
+        if not berat.isnumeric() :
+            print("mohon ulangi")
+            continue
+
+        return nama,gender,tinggi,berat
+    
+def perhitungan(berat,tinggi,nama):
+    '''hitung bmi'''
+    bmi = berat / ((tinggi/100)**2)
+    print("="*30)
+    return bmi, print(f"Halo {nama}, BMI anda: {bmi:.1f}")
+
+def kategori_l(bmi):
+    if bmi < 18.5: keterangan = "Kurus"
+    elif bmi <= 22.9: keterangan = "Ideal"
+    else: keterangan = "Obesitas"
+    return keterangan
+
+def kategori_p(bmi):
+    if bmi < 18.5: keterangan = "Kurus"
+    elif bmi <= 24.9: keterangan = "Ideal"
+    else: keterangan = "Obesitas"
+    return keterangan
+
+
+while True:
+    header()
+    nama,gender,tinggi,berat = input_user()
     tinggi_float = float(tinggi)
     berat_float = float(berat)
-
-    bmi = berat_float / ((tinggi_float/100)**2)
-    print(f"\nHalo {nama_input}, BMI anda: {bmi:.1f}")
+    bmi,tampilan = perhitungan(berat_float,tinggi_float,nama)
 
     # Menggunakan 'in' agar lebih efisien
-    if gender in ["pria", "laki laki", "cowok"]:
-        # Logika kategori pria
-        if bmi < 18.5: keterangan = "Kurus"
-        elif bmi <= 22.9: keterangan = "Ideal"
-        else: keterangan = "Obesitas"
+    if gender in ["pria", "lakilaki", "cowok"]:
+       keterangan = kategori_l(bmi)
         
     elif gender in ["wanita", "perempuan", "cewek"]:
-        # Logika kategori wanita
-        if bmi < 18.5: keterangan = "Kurus"
-        elif bmi <= 24.9: keterangan = "Ideal"
-        else: keterangan = "Obesitas"
+        keterangan = kategori_p(bmi)
     else:
         keterangan = "Data tidak valid"
         
 
-    print(f"Keterangan: {keterangan}")
+    print(f"Keterangan: {keterangan.upper()}")
+    print("="*30)
     if keluar():
         break
