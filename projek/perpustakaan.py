d1f4c5f01efc7612a4cd3184559f9cf8e37af2ec
ridwannazim <muhamadridwanwan05@gmail.com>
2026-05-08 18:57:14 +0700

    membenahi fungsi

--- projek/perpustakaan.py
+++ projek/perpustakaan.py
@@ -20,6 +20,21 @@ gudang_={
 # daftar pinjam
 pinjam = {}
 
+def header():
+    '''header utama'''
+    print("="*25)
+    print(f"{'SELAMAT DATANG':^25}")
+    print(F"{'PERPUSTAKAAN CENDIKIA':^25}")
+    print("="*25)
+    print("\t1.pinjam buku \n\t2.cek gudang \n\t3.riwayat pinjaman \n\t4.keluar")
+    print("="*25)
+
+def header_fitur(pesan):
+    '''header fitur'''
+    print("="*25)
+    print(f"{pesan:^25}")
+    print("="*25)
+
 def tanya(pesan):
     while True:
         jawab = input(pesan + " (y/n) = ").lower().strip()
@@ -28,33 +43,43 @@ def tanya(pesan):
         else:
             print("input salah!!") 
         
+def tampilan_riwayat_pinjaman():
+    no=1
+    for kategori,isi in pinjam.items():
+            print(f"{no}. kategori = {kategori.upper()}")
+            no+=1
+            nosub=1     
+            for riwayat in isi:
+                print(f"  {nosub}. judul = {riwayat['judul']:<20} jumlah = {riwayat['jumlah']:<5} waktu peminjaman = {riwayat['waktu']}")
+                nosub+=1
 def history():
     '''fitur menu riwayat pinjaman'''
     while True:
         os.system("cls")
-        print("="*5,"history pinjaman","="*5)
+        header_fitur("HISTORY PINJAMAN")
         if not pinjam :
             print("belum ada pinjaman")
         elif pinjam:
-            no=1
-            for kategori,isi in pinjam.items():
-                    print(f"{no}. kategori = {kategori.upper()}")
-                    no+=1
-                    nosub=1     
-                    for riwayat in isi:
-                        print(f"  {nosub}. judul = {riwayat['judul']:<20} jumlah = {riwayat['jumlah']:<5} waktu peminjaman = {riwayat['waktu']}")
-                        nosub+=1
-
+            print("="*87)
+            tampilan_riwayat_pinjaman()
+            print("="*87)
         if not tanya("lihat lagi?"):
             break
 
+def proses_tambah_barang(kategori,buku,jumlah):
+    if kategori in gudang_ and buku in gudang_[kategori]:
+        gudang_[kategori][buku] += jumlah
+    elif kategori in gudang_ and not buku in gudang_[kategori]:
+        gudang_[kategori][buku] = jumlah
+    elif kategori not in gudang_:
+        gudang_[kategori] = {}
+        gudang_[kategori][buku] = jumlah
+        
 def tambah_barang():
     '''fitur sub menu tambah barang'''
     while True:
         os.system("cls")
-        print("="*25)
-        print(f"{'TAMBAH BARANG':^25}")
-        print("="*25,"\n")
+        header_fitur("TAMBAH BARANG")
         tambah_kategori = input(f"{'kategori':<15}= ").title()
         tambah_buku = input(f"{'judul buku':<15}= ").title()
         jumlah_buku = input(f"{'masukan jumlah':<15}= ")
@@ -63,32 +88,27 @@ def tambah_barang():
             time.sleep(1)
             continue
         jumlah_buku=int(jumlah_buku)
-        if tambah_kategori in gudang_ and tambah_buku in gudang_[tambah_kategori]:
-            gudang_[tambah_kategori][tambah_buku] += jumlah_buku
-        elif tambah_kategori in gudang_ and not tambah_buku in gudang_[tambah_kategori]:
-            gudang_[tambah_kategori][tambah_buku] = jumlah_buku
-        elif tambah_kategori not in gudang_:
-            gudang_[tambah_kategori] = {}
-            gudang_[tambah_kategori][tambah_buku] = jumlah_buku
-            print("="*25)
+        proses_tambah_barang(tambah_kategori,tambah_buku,jumlah_buku)
+        print("="*25)
         if not tanya("\ntambah lagi?"):
             break
+
+def tampilan_stok_gudang():
+    no=1
+    for kategori,isi in gudang_.items():
+        print(f"{no}. kategori = {kategori.upper()}")
+        no+=1
+        nosub=1     
+        for judul,stok in isi.items():
+            print(f"  {nosub}. judul \t= {judul:20} stok = {stok}")
+            nosub+=1
 
 def cek_stok():
     '''fitur sub menu tambah barang'''
     while True:
         os.system("cls")
-        print("="*25)
-        print(f"{'STOK SAAT INI':^25}")
-        print("="*25)
-        no=1
-        for kategori,isi in gudang_.items():
-            print(f"{no}. kategori = {kategori.upper()}")
-            no+=1
-            nosub=1     
-            for judul,stok in isi.items():
-                print(f"  {nosub}. judul \t= {judul:20} stok = {stok}")
-                nosub+=1
+        header_fitur("STOK SAAT INI")
+        tampilan_stok_gudang()
         print("="*25)
         if tanya("keluar?"):
             break
@@ -97,9 +117,7 @@ def cek_gudang():
     '''fitur menu cek gudang'''
     while True:
         os.system("cls")
-        print("="*25)
-        print(F"{'CEK GUDANG':^25}")
-        print("="*25)
+        header_fitur("CEK GUDANG")
         menu_cek_gudang = input("\t1.cek stok \n\t2.tambah barang \n\t3.keluar  \n\npilihan = ")
         if menu_cek_gudang not in ["1","2","3"]:
             print("maaf input salah, mohon ulangi")
@@ -112,22 +130,17 @@ def cek_gudang():
         elif menu_cek_gudang == "3":
             break
 
-def pinjam_buku():
-    '''fitur menu pinjam buku'''
+def input_user_pinjam():
     while True:
-        os.system("cls")
-        print("="*25)
-        print(f"{'PINJAM BUKU':^25}")
-        print("="*25)
         print("\nketik (k) untuk keluar")
         input_kategori = input(f"  {'kategori buku':<15}= ").title()
         if input_kategori == "K":
-            break
+            return None
         elif input_kategori not in gudang_:
             print("maaf kategori tidak ditemukan")
             time.sleep(1)
             continue
-        input_judul = input(f"  {'judul buku':<15}= ").title()
+        input_judul= input(f"  {'judul buku':<15}= ").title()
         if input_judul not in gudang_[input_kategori] :
             print("maaf barang tidak tersedia")
             time.sleep(1)
@@ -137,23 +150,41 @@ def pinjam_buku():
             print("silakan isi dengan benar")
             time.sleep(1)
             continue
-        jumlah_pinjaman = int(jumlah_pinjaman)
+        jumlah_pinjaman = int(jumlah_pinjaman)  
         if jumlah_pinjaman > gudang_[input_kategori][input_judul]:
             print(f"maaf jumlah stok tidak memadai. stok saat ini = {gudang_[input_kategori][input_judul]} ")
             time.sleep(1)
             continue   
+        return input_kategori,input_judul,jumlah_pinjaman
 
-        gudang_[input_kategori][input_judul] -= jumlah_pinjaman
-        waktu_sekarang = dt.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-        riwayat_pinjaman = {
-            'judul':input_judul,
-            'jumlah':jumlah_pinjaman,
-            'waktu':waktu_sekarang
-            }
-        if input_kategori not in pinjam:
-            pinjam[input_kategori] = [riwayat_pinjaman]
-        elif input_kategori in pinjam :
-            pinjam[input_kategori].append(riwayat_pinjaman)
+def proses_tampilan_data_pinjam(kategori,judul,jumlah):
+    gudang_[kategori][judul] -= jumlah
+    waktu_sekarang = dt.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    riwayat_pinjaman = {
+        'judul':judul,
+        'jumlah':jumlah,
+        'waktu':waktu_sekarang
+        }
+    
+    return riwayat_pinjaman
+
+def pengecekan_data_buku_pinjam(kategori,riwayat):
+    if kategori not in pinjam:
+        pinjam[kategori] = [riwayat]
+    elif kategori in pinjam :
+        pinjam[kategori].append(riwayat)
+
+def pinjam_buku():
+    '''fitur menu pinjam buku'''
+    while True:
+        os.system("cls")
+        header_fitur("PINJAM BUKU")
+        hasil = input_user_pinjam()
+        if hasil is None:
+            break
+        kategori,judul,jumlah_pinjam = hasil 
+        data_pinjam_sementara = proses_tampilan_data_pinjam(kategori,judul,jumlah_pinjam)
+        pengecekan_data_buku_pinjam(kategori,data_pinjam_sementara)
         print("\ndata berhasil disimpan. semangat membaca.")
         print("="*25)
         if not tanya("pinjam lagi?"):
@@ -163,12 +194,7 @@ def pinjam_buku():
 # awal program
 while True:
     os.system("cls")
-    print("="*25)
-    print(f"{'SELAMAT DATANG':^25}")
-    print(F"{'PERPUSTAKAAN CENDIKIA':^25}")
-    print("="*25)
-    print("\t1.pinjam buku \n\t2.cek gudang \n\t3.riwayat pinjaman \n\t4.keluar")
-    print("="*25)
+    header()
     menu_utama = input("pilihanmu = ").strip()
     if menu_utama not in ["1","2","3","4"]:
         print("pilihan tidak tersedia! mohon ulangi!")
